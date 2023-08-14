88bd62e1b01e3bacd8263c6651f72b7d517f44db
Mid Hunter <midhunterx@gmail.com>
2023-08-14 12:05:52 +0530

    🧘 Added Zen mode

---
+++ lua/plugins/zen_mode.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/twilight.nvim",
+    cmd = "Twilight",
+    opts = {
+      dimming = { alpha = 0.4, },
+    }
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {}
+  },
+}
