10eeaa2b19cf793d834f8d0c1e93c64ab5f34b8d
Mid Hunter <midhunterx@gmail.com>
2023-08-15 10:56:12 +0530

    ☑️ Snippet expands only if selected explicitly

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -62,9 +62,21 @@ return{
           end, { "i", "s" }),
 
           -- ENTER TO CONFIRM SNIPPET
-           ['<CR>'] = cmp.mapping.confirm ({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true
+          --  ['<CR>'] = cmp.mapping.confirm ({
+          --   behavior = cmp.ConfirmBehavior.Insert,
+          --   select = true
+          -- }),
+
+          ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
           }),
 
         }),
