aa51527af64f9d6cb1f4b9491d9de878ad9f21a1
Mid Hunter <midhunterx@gmail.com>
2023-10-23 20:43:59 +0530

    💧 Added Oil FileManager

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return{
+  'stevearc/oil.nvim',
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    vim.keymap.set("n", "<leader>oi", function()
+      vim.cmd("vsplit | wincmd h | | vertical resize -20")
+      require("oil").open()
+    end)
+  },
+  cmd = "Oil",
+
+  config = function ()
+    require("oil").setup()
+  end,
+}
