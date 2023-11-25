4ee4bf5dc16a2364c3ddd245cca239299e71351d
Mid Hunter <midhunterx@gmail.com>
2023-11-25 09:42:06 +0530

    ⏹️ Blocky StatusLine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,6 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
+  -- For preventing loading of lualine on dashboard
   event = { "BufAdd", "BufNewFile", "BufRead" },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { }
+  opts = {
+    options = {
+      icons_enabled = true,
+      component_separators = '|',
+      section_separators = '',
+    },
+  }
 }
