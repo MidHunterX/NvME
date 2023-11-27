57ad0c8aeee15e509d70a500e138769e2d9e1e84
Mid Hunter <midhunterx@gmail.com>
2023-11-27 09:25:31 +0530

    🤩 Auto set Capabilities for AutoCompletion

--- lua/plugins/language_server_protocol.lua
+++ lua/plugins/language_server_protocol.lua
@@ -38,9 +38,14 @@ return {
     -- Automatically configures LSP servers
     -- Cannot manually configure using lspconfig if this is enabled :(
     config = function()
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       require("mason-lspconfig").setup_handlers {
         function(server_name)  -- default handler (optional)
-          require("lspconfig")[server_name].setup {}
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
         end,
       }
     end,
