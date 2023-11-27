71b48ec375de08722a9d2149a008e23672cacc1e
Mid Hunter <midhunterx@gmail.com>
2023-11-27 10:48:00 +0530

    👍 Updated Breaking changes

--- lua/plugins/indent_lines.lua
+++ lua/plugins/indent_lines.lua
@@ -1,73 +1,44 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   config = function()
-    ------------------------- NON HIGHLIGHTED INDENTS -------------------------
-    vim.cmd [[highlight IndentBlanklineIndent1 guifg=#65661a gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent2 guifg=#643266 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent3 guifg=#164a66 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent4 guifg=#336633 gui=nocombine]]
 
-    --------------------------- HIGHLIGHTED INDENTS ---------------------------
-    vim.cmd([[highlight IndentContext1 guifg=#c9cc33 gui=nocombine]])
-    vim.cmd([[highlight IndentContext2 guifg=#c964cc gui=nocombine]])
-    vim.cmd([[highlight IndentContext3 guifg=#2d94cc gui=nocombine]])
-    vim.cmd([[highlight IndentContext4 guifg=#66cc66 gui=nocombine]])
+    local non_highlight = {
+      "NonHighlight1",
+      "NonHighlight2",
+      "NonHighlight3",
+      "NonHighlight4",
+    }
 
-    require("indent_blankline").setup {
-      -- char = "│",
-      -- char_blankline = "╎", -- │ | ╎ ¦ ┆ ┊ └
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
+    local highlight = {
+      "Highlight1",
+      "Highlight2",
+      "Highlight3",
+      "Highlight4",
+    }
+
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+
+      vim.api.nvim_set_hl(0, "NonHighlight1", {fg="#65661a"})
+      vim.api.nvim_set_hl(0, "NonHighlight2", {fg="#643266"})
+      vim.api.nvim_set_hl(0, "NonHighlight3", {fg="#164a66"})
+      vim.api.nvim_set_hl(0, "NonHighlight4", {fg="#336633"})
+
+      vim.api.nvim_set_hl(0, "Highlight1", {fg="#c9cc33"})
+      vim.api.nvim_set_hl(0, "Highlight2", {fg="#c964cc"})
+      vim.api.nvim_set_hl(0, "Highlight3", {fg="#2d94cc"})
+      vim.api.nvim_set_hl(0, "Highlight4", {fg="#66cc66"})
+
+    end)
+
+    require("ibl").setup {
+      scope = {
+        highlight = highlight
       },
-      context_highlight_list = {
-        "IndentContext1",
-        "IndentContext2",
-        "IndentContext3",
-        "IndentContext4",
-      },
-      strict_tabs = true,
-      use_treesitter = true,
-      show_current_context = true,
-      show_current_context_start = true,
-      show_trailing_blankline_indent = false,
-      buftype_exclude = { "terminal", 'nofile', 'quickfix' },
-      filetype_exclude = { "dashboard", "NvimTree", "packer", "lsp-installer" },
-      context_patterns = {
-        "class", "return", "function", "method", "^if", "^while", "^for",
-        "^table", "try_statement", "if_statement", "import_statement",
-        "arguments", "with"
-
-
-        --------------------------[ PATTERN FILTER ]--------------------------
-        -- "^object", "block", "jsx_element", "jsx_self_closing_element",
-        -- "else_clause", "catch_clause", "operation_type",
-        --
-        --------------------------[ PATTERN BACKUP ]--------------------------
-        -- "class", "return", "function", "method", "^if", "^while", "jsx_element",
-        -- "^for", "^object", "^table", "block", "arguments", "if_statement",
-        -- "else_clause", "jsx_element", "jsx_self_closing_element",
-        -- "try_statement", "catch_clause", "import_statement", "operation_type"
+      indent = {
+        highlight = non_highlight
       },
     }
   end
 }
-
--- Fastest and most optimized indent plugin ever
--- return {
---   'nvimdev/indentmini.nvim',
---   event = 'BufEnter',
---   config = function()
---     require("indentmini").setup({
---       char = "│",
---       exclude = {
---         "erlang",
---         "markdown",
---       }
---     })
---     -- use comment color
---     vim.cmd.highlight("default link IndentLine Comment")
---   end,
--- }
