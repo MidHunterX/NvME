79ad732808f184050a6eee87c1cc2ca7de0c9be8
MidHunterX <midhunterx@gmail.com>
2024-02-03 08:30:24 +0530

    🎨 Added Catppuccin Colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -10,7 +10,14 @@ return {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "onedark"
+      -- vim.cmd.colorscheme "onedark"
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-macchiato"
     end,
   },
   {
