7bb7ed152a8a17def64bbc24fb72522dd6c2ae95
MidHunterX <midhunterx@gmail.com>
2024-02-13 20:57:15 +0530

    🚿 Disabled custom bg sel col

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 -- Set the selection background color
-vim.api.nvim_set_hl(0, "Visual", { bg = "#37444f" })
+-- vim.api.nvim_set_hl(0, "Visual", { bg = "#37444f" })
 -- Colour Line Number
 vim.api.nvim_set_hl(0, "LineNr", { bg = 'none', fg = "#889299" })
 -- Replace tildes at the end of the buffer
