8bb7b334efbc85aca0d601746eaf397cf98cb982
MidHunterX <midhunterx@gmail.com>
2024-02-20 18:59:41 +0530

    🌲 Treesitter: Modified Remaps for faster execution

--- lua/plugins/treesitter_text-objects.lua
+++ lua/plugins/treesitter_text-objects.lua
@@ -16,49 +16,33 @@ return {
 
           keymaps = {
             -- You can use the capture groups defined in textobjects.scm
-            ["a="] = {
-              query = "@assignment.outer",
-              desc = "Select outer part of an assignment"
-            },
             ["i="] = {
-              query = "@assignment.inner",
-              desc = "Select inner part of an assignment"
-            },
-            ["l="] = {
               query = "@assignment.lhs",
               desc = "Select left hand side of an assignment"
             },
-            ["r="] = {
+            ["a="] = {
               query = "@assignment.rhs",
               desc = "Select right hand side of an assignment"
             },
 
             -- works for javascript/typescript files
-            ["a:"] = {
-              query = "@property.outer",
-              desc = "Select outer part of an object property"
-            },
             ["i:"] = {
-              query = "@property.inner",
-              desc = "Select inner part of an object property"
-            },
-            ["l:"] = {
               query = "@property.lhs",
               desc = "Select left part of an object property"
             },
-            ["r:"] = {
+            ["a:"] = {
               query = "@property.rhs",
               desc = "Select right part of an object property"
             },
 
             -- ============================================================= --
-            ["aa"] = {
-              query = "@parameter.outer",
-              desc = "Select outer part of a parameter/argument"
-            },
             ["ia"] = {
               query = "@parameter.inner",
               desc = "Select inner part of a parameter/argument"
+            },
+            ["aa"] = {
+              query = "@parameter.outer",
+              desc = "Select outer part of a parameter/argument"
             },
 
             -- ============================================================= --
@@ -72,43 +56,43 @@ return {
             },
 
             -- ============================================================= --
-            ["al"] = {
-              query = "@loop.outer",
-              desc = "Select outer part of a loop"
-            },
             ["il"] = {
               query = "@loop.inner",
               desc = "Select inner part of a loop"
             },
+            ["al"] = {
+              query = "@loop.outer",
+              desc = "Select outer part of a loop"
+            },
 
             -- ============================================================= --
-            ["af"] = {
-              query = "@call.outer",
-              desc = "Select outer part of a function call"
-            },
             ["if"] = {
               query = "@call.inner",
               desc = "Select inner part of a function call"
             },
+            ["af"] = {
+              query = "@call.outer",
+              desc = "Select outer part of a function call"
+            },
 
             -- ============================================================= --
-            ["am"] = {
-              query = "@function.outer",
-              desc = "Select outer part of a method/function definition"
-            },
             ["im"] = {
               query = "@function.inner",
               desc = "Select inner part of a method/function definition"
             },
+            ["am"] = {
+              query = "@function.outer",
+              desc = "Select outer part of a method/function definition"
+            },
 
             -- ============================================================= --
-            ["ac"] = {
-              query = "@class.outer",
-              desc = "Select outer part of a class"
-            },
             ["ic"] = {
               query = "@class.inner",
               desc = "Select inner part of a class"
+            },
+            ["ac"] = {
+              query = "@class.outer",
+              desc = "Select outer part of a class"
             },
           },
 
@@ -128,14 +112,14 @@ return {
           enable = true,
 
           swap_next = {
-            ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-            ["<leader>n:"] = "@property.outer", -- swap object property with next
+            ["<leader>na"] = "@parameter.inner", -- swap parame/arg with next
+            ["<leader>n:"] = "@property.outer", -- swap obj property with next
             ["<leader>nm"] = "@function.outer", -- swap function with next
           },
 
           swap_previous = {
-            ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-            ["<leader>p:"] = "@property.outer", -- swap object property with prev
+            ["<leader>pa"] = "@parameter.inner", -- swap param/arg with prev
+            ["<leader>p:"] = "@property.outer", -- swap obj property with prev
             ["<leader>pm"] = "@function.outer", -- swap function with previous
           },
 
