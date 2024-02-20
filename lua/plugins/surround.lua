d0053e9fd00fe5feee0a2d9ad3de895c300fb9ac
MidHunterX <midhunterx@gmail.com>
2024-02-20 20:06:17 +0530

    🏳️ Added Nvim Surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,32 @@
+return{
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
+
+      aliases = {
+        ["a"] = ">",
+        ["b"] = ")",
+        ["B"] = "}",
+        ["r"] = "]",
+        ["q"] = { '"', "'", "`" },
+        ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+      },
+
+    })
+  end
+}
