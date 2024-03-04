b34c067ca6013ecc994a0ebe7105eb62fa385367
MidHunterX <midhunterx@gmail.com>
2024-03-04 12:04:57 +0530

    🎨 Changed Catppuccin Flavour to Frappe
    For better selection colors on a transparent or dark background

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -17,7 +17,7 @@ return {
     "catppuccin/nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-macchiato"
+      vim.cmd.colorscheme "catppuccin-frappe"
     end,
   },
   {
