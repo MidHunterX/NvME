87079b1c705f89f43088a0a9a0b6fe9579355230
MidHunterX <midhunterx@gmail.com>
2024-03-25 08:56:52 +0530

    🔭 Added Keymap Descriptions for Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,32 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
     -- FUZZY FINDER
-    { "<leader>ff", "<CMD>Telescope find_files<CR>",   mode = "n" },
+    {
+      "<leader>ff",
+      "<CMD>Telescope find_files<CR>",
+      mode = "n",
+      desc = "Telescope Fuzzy Find Files"
+    },
     -- GET GREP
-    { "<leader>gg", "<CMD>Telescope live_grep<CR>",    mode = "n" },
+    {
+      "<leader>gg",
+      "<CMD>Telescope live_grep<CR>",
+      mode = "n",
+      desc = "Telescope Live GREP"
+    },
     -- PROJECT VISIT BUFFERS
-    { "<leader>pv", "<CMD>Telescope buffers<CR><Esc>", mode = "n" },
+    {
+      "<leader>pv",
+      "<CMD>Telescope buffers<CR><Esc>",
+      mode = "n",
+      desc = "Telescope Buffer List"
+    },
+    -- MARKS LIST
+    {
+      "<leader>m",
+      "<CMD>Telescope marks<CR>",
+      mode = "n",
+      desc = "Telescope Marks List"
+    }
   },
 }
