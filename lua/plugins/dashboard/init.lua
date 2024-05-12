55d08beee9df39fe084c73997f4e3f0bbe7f5558
MidHunterX <midhunterx@gmail.com>
2024-05-13 01:42:29 +0530

    🍱 Fixed: Dashboard Config

--- lua/plugins/dashboard/init.lua
+++ lua/plugins/dashboard/init.lua
@@ -7,46 +7,63 @@ end
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
-  opts = {
-    theme = 'hyper',     --  theme is doom and hyper default is hyper
-    disable_move = true, -- boolean default is false disable move key
-    config = {
-      header = MidHunterX_UI(),
-      ---------- [ACTIONS] ----------
-      -- lua vim.api.nvim_command("!<shell cmd>")
-      shortcut = {
-        {
-          desc = '󰊳 Netrw',
-          group = 'Number',
-          action = 'lua vim.api.nvim_command("Ex")',
-          key = 'f'
-        },
-        {
-          desc = ' Telescope',
-          group = 'DiagnosticHint',
-          action = 'Telescope find_files',
-          key = 'd',
-        },
-        {
-          desc = ' Grep',
-          group = 'Label',
-          action = 'Telescope live_grep',
-          key = 's',
-        },
-        {
-          desc = ' Random',
-          group = 'Debug',
-          action = 'lua print(math.random(1, 100))',
-          key = 'r',
-        },
+  config = function ()
+    -- General
+    vim.cmd([[highlight DashboardHeader guifg=#b5bfe2 gui=nocombine]])
+    -- Hyper theme
+    vim.cmd([[highlight DashboardProjectTitle guifg=#89b4fa gui=nocombine]])
+    vim.cmd([[highlight DashboardMruTitle guifg=#89b4fa gui=nocombine]])
+    vim.cmd([[highlight DashboardFiles guifg=#cdd6f4 gui=nocombine]])
+
+    require('dashboard').setup {
+      theme = 'hyper',     --  theme is doom and hyper default is hyper
+      disable_move = false, -- boolean default is false
+      hide = {
+        statusline = false,
+        tabline = false,
       },
-      -- Most Recent Files
-      mru = {
-        limit = 2,
-        icon = ' ',
-        label = 'Recent Files',
-      },
-      footer = {}
+      config = {
+        header = MidHunterX_UI(),
+        shortcut = {
+          {
+            desc = ' Netrw',
+            group = 'Number',
+            action = 'lua vim.api.nvim_command("Ex")',
+            key = 'x'
+          },
+          {
+            desc = ' Fuzzy Find',
+            group = 'DiagnosticHint',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' Grep Search',
+            group = 'Label',
+            action = 'Telescope live_grep',
+            key = 'g',
+          },
+          {
+            desc = ' Random',
+            group = 'Debug',
+            action = 'lua print(math.random(1, 999))',
+            key = 'r',
+          },
+        },
+        -- Project List
+        project = {
+          limit = 4,
+          icon = '  ',
+          label = 'Recent Projects',
+        },
+        -- Most Recent Files
+        mru = {
+          limit = 3,
+          icon = '  ',
+          label = 'Recent Files',
+        },
+        footer = {''}
+      }
     }
-  },
+  end,
 }
