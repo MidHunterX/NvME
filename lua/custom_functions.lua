ca6060f7c50ee6d23cefc911900798e43b6fa24f
MidHunterX <midhunterx@gmail.com>
2024-05-12 09:34:13 +0530

    🏃 Added: Run C Support

--- lua/custom_functions.lua
+++ lua/custom_functions.lua
@@ -82,14 +82,21 @@ vim.cmd('nnoremap <leader>gc :lua GitAddAndCommit()<CR>')
 
 function Execute_order_69()
   local file_type = vim.bo.filetype
+  -- PYTHON SCRIPT
   if file_type == 'python' then
     vim.cmd(':vs')
     vim.cmd(':term python %')
+  -- HTML SERVER
   elseif file_type == "html" then
     vim.cmd(":term live-server --no-browser")
+  -- BASH SCRIPT
   elseif file_type == "sh" then
     vim.cmd(':vs')
     vim.cmd(":term bash %")
+  -- C PROGRAMMING LANGUAGE
+  elseif file_type == 'c' then
+    vim.cmd(':vs')
+    vim.cmd(":term gcc % && ./a.out")
   else
     print('This file?... Cannot run because no.')
   end
