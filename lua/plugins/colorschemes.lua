cf250bb5bec63d68898009d8ecfda9ffccb9e09a
MidHunterX <midhunterx@gmail.com>
2024-05-15 20:21:32 +0530

    🎨 Delet: Unwanted Colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,30 +1,9 @@
 return {
   {
-    "Mofiqul/vscode.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme "vscode"
-    end,
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme "onedark"
-    end,
-  },
-  {
     "catppuccin/nvim",
     priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin-frappe"
-    end,
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      -- vim.cmd.colorscheme "rose-pine"
     end,
   },
 }
