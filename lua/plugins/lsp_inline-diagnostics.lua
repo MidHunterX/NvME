5e41184f93a217c8e18c574a7dae552aebb93436
MidHunterX <midhunterx@gmail.com>
2025-01-11 09:40:35 +0530

    🛎️ LSP: Responsive Diagnostic virtual_text

---
+++ lua/plugins/lsp_inline-diagnostics.lua
@@ -0,0 +1,36 @@
+-- Disable neovim's diagnostic virtual_text
+vim.diagnostic.config({ virtual_text = false })
+
+return {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  -- priority = 1000,
+  config = function()
+    require('tiny-inline-diagnostic').setup({
+      options = {
+        show_source = true,
+        use_icons_from_diagnostic = false,
+
+        -- virtual_text settings for tiny-inline-diagnostic
+        multilines = {
+          enabled = true,
+          always_show = true,
+        },
+      },
+
+      -- Overwrites preset
+      signs = {
+        left = "",
+        right = "",
+        diag = "●",
+        arrow = "    ",
+        up_arrow = "    ",
+        vertical = " │",
+        vertical_end = " └",
+      },
+      blend = {
+        factor = 0.25,
+      },
+    })
+  end
+}
