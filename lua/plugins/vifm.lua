36acbad1784deeab5101dbc62c54140f6fcf1de7
MidHunterX <midhunterx@gmail.com>
2025-02-27 13:30:29 +0530

    📂 Vifm: Tried to replace with Snacks

--- lua/plugins/vifm.lua
+++ lua/plugins/vifm.lua
@@ -1,3 +1,6 @@
+-- Cannot be replaced by Snacks.terminal yet
+-- as it can be used to load file into nvim buffer as well
+
 -- local setting = "-c 'set vifminfo-=savedirs,dirstack,state,tui' "
 -- local command = "-c only"
 
