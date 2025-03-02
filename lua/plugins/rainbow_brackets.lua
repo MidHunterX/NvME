014890f56fe89416a743381a1e6d5bd07634268c
MidHunterX <midhunterx@gmail.com>
2025-03-02 18:31:14 +0530

    🌈 Delimeter: Updated referencing Docs

--- lua/plugins/rainbow_brackets.lua
+++ lua/plugins/rainbow_brackets.lua
@@ -5,16 +5,23 @@ return {
     vim.cmd([[highlight RainbowBracket2 guifg=#d78cd9 gui=nocombine]])
     vim.cmd([[highlight RainbowBracket3 guifg=#82c1e3 gui=nocombine]])
     vim.cmd([[highlight RainbowBracket4 guifg=#8cd98c gui=nocombine]])
+
     -- This module contains a number of default definitions
     local rainbow_delimiters = require 'rainbow-delimiters'
 
     vim.g.rainbow_delimiters = {
       strategy = {
+        -- Use global strategy by default
         [''] = rainbow_delimiters.strategy['global'],
+        -- Use local strategy
         vim = rainbow_delimiters.strategy['local'],
+        html = rainbow_delimiters.strategy['local'],
+        htmldjango = rainbow_delimiters.strategy['local'],
       },
       query = {
+        -- Use parentheses by default
         [''] = 'rainbow-delimiters',
+        -- Use blocks for Lua
         lua = 'rainbow-blocks',
       },
       highlight = {
