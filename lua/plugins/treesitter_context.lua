0b5221bda8e82764d6b9756e5fd709bb280fef17
MidHunterX <midhunterx@gmail.com>
2025-03-03 11:04:07 +0530

    🌲 Treesitter: Fixed multiline context not initializing

--- lua/plugins/treesitter_context.lua
+++ lua/plugins/treesitter_context.lua
@@ -1,12 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   event = "VeryLazy",
-  config = function ()
+  config = function()
     vim.cmd([[highlight TreesitterContextBottom guisp=#45475a]])
+    require 'treesitter-context'.setup({
+      enable = true,
+      max_lines = 5,         -- 0 for no limit
+      multiline_threshold = 1, -- Max lines to show for single context
+      mode = 'cursor',       -- Choices: 'cursor', 'topline'
+      separator = nil,       -- Choices: nil | '-'
+    })
   end,
-  opts = {
-    max_lines = 5,    -- 0 for no limit
-    mode = 'cursor', -- Choices: 'cursor', 'topline'
-    separator = nil,  -- Choices: nil | '-'
-  },
 }
