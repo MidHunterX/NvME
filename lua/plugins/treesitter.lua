e4407c96d001e6b4070e2c31fbdf2754b6d22862
MidHunterX <midhunterx@gmail.com>
2025-03-17 09:55:34 +0530

    🌳 TS: Fix flash f/t motion conflict
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -316,17 +316,21 @@ return {
     -- █▀█ █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀   █▀▄▀█ █▀█ █░█ █▀▀
     -- █▀▄ ██▄ █▀▀ ██▄ █▀█ ░█░   █░▀░█ █▄█ ▀▄▀ ██▄
 
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    local move = require("nvim-treesitter.textobjects.repeatable_move")
 
     -- vim way: ; goes to the direction you were moving.
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    vim.keymap.set({ "n", "x", "o" }, ";", move.repeat_last_move)
+    vim.keymap.set({ "n", "x", "o" }, ",", move.repeat_last_move_opposite)
 
     -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+    -- WARN: Disabling bc this conflicts and breaks flash.nvim f/t motions
+    -- But only when opening file from dashboard via fuzzy finding
+
+    -- vim.keymap.set({ "n", "x", "o" }, "f", move.builtin_f_expr, { expr = true })
+    -- vim.keymap.set({ "n", "x", "o" }, "F", move.builtin_F_expr, { expr = true })
+    -- vim.keymap.set({ "n", "x", "o" }, "t", move.builtin_t_expr, { expr = true })
+    -- vim.keymap.set({ "n", "x", "o" }, "T", move.builtin_T_expr, { expr = true })
+
   end,
 
   dependencies = {
