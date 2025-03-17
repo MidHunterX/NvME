173d2826bde7783e5c222e6bd3d19c1dbeb18a8f
MidHunterX <midhunterx@gmail.com>
2025-03-17 11:30:31 +0530

    🔧 Fix numberlines on floating buffers
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- lua/auto_commands.lua
+++ lua/auto_commands.lua
@@ -28,8 +28,24 @@ au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeou
 vim.cmd([[nnoremap <expr> j line(".") == line('$') ? '<C-e>':'j']])
 
 -- Automatically change number based on Insert Mode
-vim.cmd([[autocmd InsertEnter * :set norelativenumber]])
-vim.cmd([[autocmd InsertLeave * :set relativenumber]])
+-- vim.cmd([[autocmd InsertEnter * :set norelativenumber]])
+-- vim.cmd([[autocmd InsertLeave * :set relativenumber]])
+
+-- Automatically change number based on Insert Mode, except in floating windows
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  callback = function()
+    if vim.api.nvim_win_get_config(0).relative == "" then
+      vim.opt_local.relativenumber = false
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    if vim.api.nvim_win_get_config(0).relative == "" then
+      vim.opt_local.relativenumber = true
+    end
+  end,
+})
 
 -- Yank into system clipboard "+"
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
