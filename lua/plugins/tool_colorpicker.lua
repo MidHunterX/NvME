6264fe07fae16813d4d11184d63641d60a2b8305
MidHunterX <midhunterx@gmail.com>
2025-03-17 12:23:18 +0530

    🎨 Colorpicker: HSL as Default color model
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- lua/plugins/tool_colorpicker.lua
+++ lua/plugins/tool_colorpicker.lua
@@ -1,5 +1,17 @@
 return {
   "uga-rosa/ccc.nvim",
   cmd = "CccPick",
-  opts = { },
+  config = function()
+    local ccc = require("ccc")
+    ccc.setup({
+      bar_char = "█",
+      point_char = "◊",
+      inputs = {
+        ccc.input.hsl,
+        ccc.input.rgb,
+        ccc.input.cmyk,
+      }
+    })
+  end,
+  -- Test: #fff
 }
