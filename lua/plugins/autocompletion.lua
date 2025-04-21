6128bdc128ab75ad2f073240926fd36aa3ca3104
MidHunterX <midhunterx@gmail.com>
2025-04-21 18:08:23 +0530

    🛺 CMP: Fix Snippet not exiting on Esc
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -28,6 +28,18 @@ local function confirm_if_menu(fallback)
   end
 end
 
+-- BUGFIX: stop snippets when you leave to normal mode
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
 
 return {
   --=====================================================[ @COMPLETION_ENGINE ]
