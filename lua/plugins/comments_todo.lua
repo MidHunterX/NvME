5610197b248fa910351a221ada867ea9a3289e26
MidHunterX <midhunterx@gmail.com>
2025-05-04 10:08:23 +0530

    💄 style(todo): better todo comment icons

--- lua/plugins/comments_todo.lua
+++ lua/plugins/comments_todo.lua
@@ -5,16 +5,16 @@ return {
     signs = true,
     keywords = {
       FIX = {
-        icon = " ",
+        icon = "",
         color = "error",
         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
       },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning" },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      TODO = { icon = "", color = "info" },
+      HACK = { icon = "", color = "warning" },
+      WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = "", color = "hint", alt = { "INFO" } },
+      TEST = { icon = "", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
     },
   }
 }
