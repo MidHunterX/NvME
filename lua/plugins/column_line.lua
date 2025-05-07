c7bc314cae912f2230b6be5de312f68f75fd2edf
MidHunterX <midhunterx@gmail.com>
2025-05-07 10:44:11 +0530

    🐛 fix(column_line): obtrusive highlight color

--- lua/plugins/column_line.lua
+++ lua/plugins/column_line.lua
@@ -1,6 +1,9 @@
+vim.api.nvim_set_hl(0, "VirtColumn", {fg="#45475a"}) -- #45475a #585b70
+
 return {
   "lukas-reineke/virt-column.nvim",
   opts = {
     char = "│", -- │┃
+    highlight = "VirtColumn",
   },
 }
