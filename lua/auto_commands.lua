bd9b90d70302846785b48b853244c2450d5e6e9d
MidHunterX <midhunterx@gmail.com>
2025-05-19 09:54:51 +0530

    ⏪ revert(linenr): disabled dynamic line number mode

--- lua/auto_commands.lua
+++ lua/auto_commands.lua
@@ -1,20 +1,3 @@
-
---===========================[ @AUTO_SHIFT_WIDTH ]===========================--
-
--- local fn = vim.fn
--- local autocmd = vim.api.nvim_create_autocmd
--- local augroup = vim.api.nvim_create_augroup
--- local general = augroup("General Settings", { clear = true })
-
--- autocmd("FileType", {
---   pattern = { "c", "cpp", "py", "java", "cs" },
---   callback = function()
---     vim.bo.shiftwidth = 4
---   end,
---   group = general,
---   desc = "Set shiftwidth to 4 in these filetypes",
--- })
-
 --===========================[ @GENERAL_COMMANDS ]===========================--
 -- Use system clipboard
 vim.cmd(":set clipboard=unnamedplus")
@@ -27,11 +10,13 @@ au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeou
 -- Scroll past last line
 vim.cmd([[nnoremap <expr> j line(".") == line('$') ? '<C-e>':'j']])
 
--- Automatically change number based on Insert Mode
--- vim.cmd([[autocmd InsertEnter * :set norelativenumber]])
--- vim.cmd([[autocmd InsertLeave * :set relativenumber]])
-
 -- Automatically change number based on Insert Mode, except in floating windows
+-- Philosophy:
+-- When typing (insert mode), it should look like every other editors out there
+-- But on navigation (normal mode), line numbers should serve a purpose (feature)
+-- "Insert mode is for writing - don't distract me. Normal mode is for navigating - give me tools."
+-- STATUS: Disabling due to optimization reasons
+--[[
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
   callback = function()
     if vim.api.nvim_win_get_config(0).relative == "" then
@@ -46,9 +31,8 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
     end
   end,
 })
+]]
 
 -- Yank into system clipboard "+"
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-
