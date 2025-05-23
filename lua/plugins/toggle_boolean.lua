9b9974a26016df986aea54844831256b43a43c78
MidHunterX <midhunterx@gmail.com>
2025-05-23 09:35:30 +0530

    ⚡️ perf: load toggle_boolean on keymap only

--- lua/plugins/toggle_boolean.lua
+++ lua/plugins/toggle_boolean.lua
@@ -1,18 +1,24 @@
 return {
   "nat-418/boole.nvim",
+  keys = {
+    { "<C-a>", "<Cmd>Boole Increment<CR>", desc = "Boole: Increment" },
+    { "<C-x>", "<Cmd>Boole Decrement<CR>", desc = "Boole: Decrement" },
+  },
   opts = {
     mappings = {
       increment = '<C-a>',
       decrement = '<C-x>'
     },
 
+    -- Case sensitive
     additions = {
-      {'Foo', 'Bar'},
-      {'tic', 'tac', 'toe'}
+      { 'Foo', 'Bar' },
+      { 'tic', 'tac', 'toe' }
     },
 
+    -- Case adaptive
     allow_caps_additions = {
-      {'enable', 'disable'}
+      { 'enable', 'disable' }
       -- enable → disable
       -- Enable → Disable
       -- ENABLE → DISABLE
