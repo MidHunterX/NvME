7d625277c985fb7f47a42a7c7af4c345a1c05a1a
MidHunterX <midhunterx@gmail.com>
2025-07-19 09:23:37 +0530

    🧹 chore(colorscheme): add onedark x catppuccin

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -33,4 +33,34 @@ return {
     end,
 
   },
+
+  --[[ {
+    -- OneDark with Catppuccin colors
+    -- OneDark is so good at color consistency when it comes to syntax highlighting
+    -- Catppuccin has nice bright colors
+    -- navarasu/onedark.nvim doesn't work well with render-markdown.nvim though
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        style = 'dark',
+        transparent = false,
+        cmp_itemkind_reverse = false,
+        colors = {
+          fg = "#cdd6f4",
+          light_gray = "#bac2de",
+          gray = "#a6adc8",
+          red = "#f38ba8",
+          cyan = "#94e2d5",
+          yellow = "#f9e2af",
+          orange = "#fab387",
+          green = "#a6e3a1",
+          blue = "#89b4fa",
+          purple = "#cba6f7",
+        }
+      }
+      require('onedark').load()
+    end
+  }, ]]
+
 }
