560313045495cd52e3492fb146e50cc67d0615ca
MidHunterX <midhunterx@gmail.com>
2025-08-31 04:36:09 +0530

    🐛 fix: add missing icon for WTF

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -14,6 +14,7 @@ return {
 
     icons = {
       rules = {
+        { plugin = "wtf", icon = "󰚩", color = "red" },
         { plugin = "treesj", icon = "󰃻 ", color = "blue" },
         { pattern = "fold", icon = "", color = "yellow" },
       }
