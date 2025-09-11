ee41e955fa520c455b15667044b2d8204811df40
MidHunterX <midhunterx@gmail.com>
2025-09-11 07:31:55 +0530

    ✨ feat(surround): add better semantic bracket surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,29 +1,43 @@
-return{
+return {
   "kylechui/nvim-surround",
   event = "VeryLazy",
   config = function()
     require("nvim-surround").setup({
 
       keymaps = {
-        -- insert = "<C-g>s",
-        -- insert_line = "<C-g>S",
-        normal = "ys",
-        normal_cur = "yss",
-        normal_line = "yS",
-        normal_cur_line = "ySS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "ds",
+        -- [g]o [s]urround {<motions> <bracket>}
+        normal = "gs",
+        normal_line = "gS",
+
+        -- [g]o [s]urround this [l]ine with {<bracket>}
+        normal_cur = "gsl",
+        normal_cur_line = "gsL",
+
+        -- [c]hange [s]urrounding {<bracket>}
         change = "cs",
         change_line = "cS",
+
+        -- [d]elete [s]urrounding {<bracket>}
+        delete = "ds",
+
+        -- insert = "<C-g>s",
+        -- insert_line = "<C-g>S",
+        -- visual = "S",
+        -- visual_line = "gS",
       },
 
       aliases = {
+        -- angle bracket
         ["a"] = ">",
+        -- bracket (default brackets.. the paranthesis!)
         ["b"] = ")",
+        -- brace
         ["B"] = "}",
+        -- rectangle bracket
         ["r"] = "]",
+        -- quote
         ["q"] = { '"', "'", "`" },
+        -- surrounding
         ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
       },
 
