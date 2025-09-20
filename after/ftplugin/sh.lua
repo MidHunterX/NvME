54073119ec5b4182559d58827923f3fbeb2a7692
MidHunterX <midhunterx@gmail.com>
2025-09-20 09:49:47 +0530

    🐛 fix(after): fix sh malformed print macro

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,7 +1,3 @@
-vim.bo.expandtab = true
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-
 -- Printf Debug
 local esc = string.char(27)
-vim.fn.setreg("l", "yoecho \"" .. esc .. "pA: ${" .. esc .. "pA}\"" .. esc)
+vim.fn.setreg("l", "yoecho \"" .. esc .. "pa: ${" .. esc .. "pa}\"" .. esc)
