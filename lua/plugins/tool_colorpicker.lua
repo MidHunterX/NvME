8ab0c80587716254d2b5ccd5dfdaa2117e987b4d
MidHunterX <midhunterx@gmail.com>
2025-10-05 13:20:35 +0530

    feat(colorpicker): add color picker modes

--- lua/plugins/tool_colorpicker.lua
+++ lua/plugins/tool_colorpicker.lua
@@ -4,12 +4,37 @@ return {
   config = function()
     local ccc = require("ccc")
     ccc.setup({
-      bar_char = "█",
-      point_char = "◊",
+      preserve = true,
+      bar_char = "▇", -- 🭸 █ ▇ ▆
+      point_char = "◊", -- ◊ 🮮
+      empty_point_bg = true,
       inputs = {
+        -- ccc.input.hsv,
         ccc.input.hsl,
         ccc.input.rgb,
         ccc.input.cmyk,
+        -- ccc.input.hwb,
+        ccc.input.lab,
+        -- ccc.input.lch,
+        -- ccc.input.oklab,
+        -- ccc.input.oklch,
+        -- ccc.input.hsluv,
+        -- ccc.input.okhsl,
+        -- ccc.input.okhsv,
+        -- ccc.input.xyz,
+      },
+      outputs = {
+        ccc.output.hex,
+        ccc.output.hex_short,
+        ccc.output.css_rgb,
+        ccc.output.css_rgba,
+        -- ccc.output.css_hsl,
+        -- ccc.output.css_hwb,
+        -- ccc.output.css_lab,
+        -- ccc.output.css_lch,
+        -- ccc.output.css_oklab,
+        -- ccc.output.css_oklch,
+        -- ccc.output.float,
       }
     })
   end,
