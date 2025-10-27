75cc6e9f6c0697c1ee2e1bc4ae9869a6cde9e599
MidHunterX <midhunterx@gmail.com>
2025-10-27 09:32:46 +0530

    feat(ftplugin): add markdown checkbox on newline

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -87,7 +87,13 @@ vim.keymap.set("v", "<C-l>", make_markdown_link,
 
 
 -- MARKDOWN LIST ITEMS (using commentstrings)
-vim.opt_local.comments = { "b:*", "b:-", "b:+", "n:>" }
+vim.opt_local.comments = {
+  "b:*",
+  "b:- [ ]",
+  "b:-",
+  "b:+",
+  "n:>",
+}
 -- r → Continues the comment when pressing <Enter> inside a comment.
 -- o → Continues the comment when opening a new line with o or O.
 vim.opt_local.formatoptions:append("ro")
