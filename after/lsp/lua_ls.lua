241da20ed78640bfb42b371a6f8bf3675121ac26
MidHunterX <midhunterx@gmail.com>
2026-01-24 18:44:24 +0530

    🐛 fix(lsp): lsp_lua loads vim context all the time

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -12,7 +12,7 @@ return {
         -- This includes sources from everything loaded in runtime, which you
         -- should not need unless you are actively developing nvim or plugins.
         -- Downside: Constant diagnosing workspace and slow performance
-        library = vim.api.nvim_get_runtime_file("", true),
+        -- library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false,
       },
       telemetry = { enable = false },
