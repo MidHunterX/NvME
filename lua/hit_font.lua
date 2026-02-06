8cfef2665ab17249bdd0defedc565d58365f7db6
MidHunterX <midhunterx@gmail.com>
2026-02-06 13:34:33 +0530

    ✨ feat: add leetspeak feature

--- lua/hit_font.lua
+++ lua/hit_font.lua
@@ -1,62 +1,60 @@
-local function data()
-  return {
-    "█▀█",
-    "▀▀█",
-    "█░█░█",
-    "▀▄▀▄▀",
-    "█▀▀",
-    "██▄",
-    "█▀█",
-    "█▀▄",
-    "▀█▀",
-    "░█░",
-    "█▄█",
-    "░█░",
-    "█░█",
-    "█▄█",
-    "█",
-    "█",
-    "█▀█",
-    "█▄█",
-    "█▀█",
-    "█▀▀",
-    "▄▀█",
-    "█▀█",
-    "█▀",
-    "▄█",
-    "█▀▄",
-    "█▄▀",
-    "█▀▀",
-    "█▀░",
-    "█▀▀",
-    "█▄█",
-    "█░█",
-    "█▀█",
-    "░░█",
-    "█▄█",
-    "█▄▀",
-    "█░█",
-    "█░░",
-    "█▄▄",
-    "▀█",
-    "█▄",
-    "▀▄▀",
-    "█░█",
-    "█▀▀",
-    "█▄▄",
-    "█░█",
-    "▀▄▀",
-    "█▄▄",
-    "█▄█",
-    "█▄░█",
-    "█░▀█",
-    "█▀▄▀█",
-    "█░▀░█",
-  }
-end
+local data_hitfont = {
+  "█▀█",
+  "▀▀█",
+  "█░█░█",
+  "▀▄▀▄▀",
+  "█▀▀",
+  "██▄",
+  "█▀█",
+  "█▀▄",
+  "▀█▀",
+  "░█░",
+  "█▄█",
+  "░█░",
+  "█░█",
+  "█▄█",
+  "█",
+  "█",
+  "█▀█",
+  "█▄█",
+  "█▀█",
+  "█▀▀",
+  "▄▀█",
+  "█▀█",
+  "█▀",
+  "▄█",
+  "█▀▄",
+  "█▄▀",
+  "█▀▀",
+  "█▀░",
+  "█▀▀",
+  "█▄█",
+  "█░█",
+  "█▀█",
+  "░░█",
+  "█▄█",
+  "█▄▀",
+  "█░█",
+  "█░░",
+  "█▄▄",
+  "▀█",
+  "█▄",
+  "▀▄▀",
+  "█░█",
+  "█▀▀",
+  "█▄▄",
+  "█░█",
+  "▀▄▀",
+  "█▄▄",
+  "█▄█",
+  "█▄░█",
+  "█░▀█",
+  "█▀▄▀█",
+  "█░▀░█",
+}
 
 local function HitList()
-  local lines = data()
+  local lines = data_hitfont
   local characters = {}
   for i = 1, #lines, 2 do
     local character = lines[i] .. "\n" .. lines[i + 1]
@@ -114,11 +112,32 @@ function HitFont()
   botrow = botrow:gsub("^%s*(.-)%s*$", "%1")
 
   -- INSERT FONT IN NEOVIM BUFFER
-  vim.api.nvim_buf_set_lines(0, line_number - 1, line_number, false, {toprow})
-  vim.api.nvim_buf_set_lines(0, line_number, line_number, false, {botrow})
-
+  vim.api.nvim_buf_set_lines(0, line_number - 1, line_number, false, { toprow })
+  vim.api.nvim_buf_set_lines(0, line_number, line_number, false, { botrow })
 end
 
--- HitFont()
-vim.cmd("command! HitFont lua HitFont()")
+function LeetSpeak()
+  local plain = "qwertyuiopasdfghjklzxcvbnm"
+  local cipher = "QW3R7YU10P45DF6HJKLZXCVBNM"
 
+  local line_number = vim.fn.line('.')
+  local sentence = vim.fn.getline(line_number)
+
+  local leet_sentence = ""
+
+  for i = 1, #sentence do
+    local letter = sentence:sub(i, i)
+    local array_id = plain:find(letter, 1, true)
+    if array_id then
+      leet_sentence = leet_sentence .. cipher:sub(array_id, array_id)
+    else
+      leet_sentence = leet_sentence .. letter
+    end
+  end
+
+  vim.fn.setline(line_number, leet_sentence)
+end
+
+-- COMMAND MAPPINGS
+vim.cmd("command! HitFont lua HitFont()")
+vim.cmd("command! LeetSpeak lua LeetSpeak()")
