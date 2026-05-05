533ef4eb16cf0f5bfc7a2191efe91fa9457d4f62
MidHunterX <midhunterx@gmail.com>
2026-05-05 09:35:28 +0530

    ⚡️ perf: improve matchup efficiency and bugfixes

--- lua/plugins/bracket_matchit.lua
+++ lua/plugins/bracket_matchit.lua
@@ -1,23 +1,25 @@
 return {
   'andymass/vim-matchup',
   init = function()
-    -- modify your configuration vars here
-    vim.g.matchup_treesitter_stopline = 500
+    -- perf: lines to search for highlighting
+    vim.g.matchup_treesitter_stopline = 69
+    -- fix: lualine statusline flicker
+    vim.g.matchup_matchparen_offscreen = {}
+    -- fix: nvim-cmp lsp entry getting autoselected
+    vim.g.matchup_matchparen_pumvisible = 0
 
-    -- or call the setup function provided as a helper. It defines the
-    -- configuration vars for you
-    require('match-up').setup({
-      treesitter = {
-        stopline = 500
-      }
+    -- feat: auto edit closing tags just by normal editing
+    vim.g.matchup_transmute_enabled = 1
+
+    -- feat: highlight tags only
+    vim.g.matchup_matchpref = {
+      html = { tagnameonly = 1 }
+    }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "htmldjango", "django", "twig", "eruby" },
+      callback = function()
+        vim.b.match_words = vim.fn["matchup#util#standard_html"]()
+      end,
     })
   end,
-  -- or use the `opts` mechanism built into `lazy.nvim`. It calls
-  -- `require('match-up').setup` under the hood
-  ---@type matchup.Config
-  opts = {
-    treesitter = {
-      stopline = 500,
-    }
-  }
 }
